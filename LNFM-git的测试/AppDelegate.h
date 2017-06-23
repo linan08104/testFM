8ba99d1809e18d1ca704a0164b4a429fcd6c2195
linan08104 <linan08104@163.com>
2017-06-23 22:36:58 +0800

    提交代码

---
+++ LNFM-git的测试/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LNFM-git的测试
+//
+//  Created by apple on 2017/6/23.
+//  Copyright © 2017年 linan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
